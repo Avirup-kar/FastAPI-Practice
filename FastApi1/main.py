3d8d1485ebb5295be705fea2d541b84b0bbcb861
Avirup-kar <avirupk57@gmail.com>
2026-09-24 22:11:00 +0530

    craete first-server

--- FastApi1/main.py
+++ FastApi1/main.py
@@ -9,3 +9,10 @@ def home():
 @app.get("/about")
 def home():
     return {"message": "Welcome to About"}
+
+#Users Route
+@app.get("/users")
+def users():
+    return {
+    "users": ["Mohit", "Rohit", "Amit"]
+    }    
