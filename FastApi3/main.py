398b4c9071f2454b64699db4ce2dcdffe196fb14
Avirup-kar <avirupk57@gmail.com>
2026-09-25 01:18:11 +0530

    craete FastApi2

--- FastApi3/main.py
+++ FastApi3/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# @app.post("/create_user")
+# def create_user(name: str, age: int):
+#     return {"name": name, "age": age}
+
 @app.post("/create_user")
-def create_user(name: str, age: int):
-    return {"name": name, "age": age}
+def create_user(user:dict):
+    return {
+        "Messaeg": "User created successfuly",
+        "Data": user
+    }
