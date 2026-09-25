6e8785634d6bbb6a300a9f7da87b46b43fe88501
Avirup-kar <avirupk57@gmail.com>
2026-09-25 21:45:42 +0530

    craete FastApi4

--- FastApi5/main.py
+++ FastApi5/main.py
@@ -29,4 +29,19 @@ def get_todo(todo_id: int):
     for todo in todos:
         if todo.id == todo_id:
             return todo
+    return {"err": "todo not found"}       
+
+@app.put("/todo/update/{todo_id}") 
+def update_todo(todo_id: int, updated_todo: Todo):
+    for index,todo in enumerate(todos):
+        if todo.id == todo_id:
+            todos[index] = updated_todo
+            return todos
     return {"err": "todo not found"}        
+
+@app.delete("/todo/delet/{todo_id}")
+def delet_todo(todo_id: int):
+     for index,todo in enumerate(todos):
+            if todo.id == todo_id:
+                todos.pop(index)
+                return todos
