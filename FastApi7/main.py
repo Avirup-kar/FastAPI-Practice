4633e4a31541b3136f3dd026500ab5804db24b0b
Avirup-kar <avirupk57@gmail.com>
2026-09-26 01:34:04 +0530

    craete FastApi7

--- FastApi7/main.py
+++ FastApi7/main.py
@@ -7,3 +7,16 @@ class User(BaseModel):
     name: str
     age: int
     password: str
+
+class userResponce(BaseModel):
+    name: str
+    age: int  
+
+
+@app.get("/user", response_model= userResponce)
+def get_user():
+    return {
+        "name": "Avirup kar",
+        "age": 19,
+        "password": 1234567
+    }
