1f123314ab54b1e24337b3d2fcb16478f79647be
Avirup-kar <avirupk57@gmail.com>
2026-09-25 23:14:31 +0530

    craete FastApi6

--- FastApi6/main.py
+++ FastApi6/main.py
@@ -12,11 +12,12 @@ class User(BaseModel):
 @app.post("/users")
 def create_user(user: User):
      users.append(user) 
+     return users
 
 
 @app.put("/users/{user_id}")
 def update_user(user_id: int, user: User, notify: bool = False):
-     if user_id > len(users):
+     if user_id < len(users):
           users[user_id] = user       
           return {
                "Message": "User updated",
