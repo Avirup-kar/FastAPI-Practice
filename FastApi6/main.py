333097cd2ae300ba70b4919241c952078fcd7ed2
Avirup-kar <avirupk57@gmail.com>
2026-09-25 22:55:46 +0530

    craete FastApi6

--- FastApi6/main.py
+++ FastApi6/main.py
@@ -11,4 +11,10 @@ class User(BaseModel):
 
 @app.post("/users")
 def create_user(user: User):
-     users .append(user)   
+     users.append(user) 
+
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, user: User, notify: bool = False):
+     if user_id > len(users):
+          users[user_id] = user       
