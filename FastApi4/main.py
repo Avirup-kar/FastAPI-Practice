804672a414343f50dacac80100b9ba4d9cdc5540
Avirup-kar <avirupk57@gmail.com>
2026-09-25 17:59:10 +0530

    craete FastApi4

--- FastApi4/main.py
+++ FastApi4/main.py
@@ -25,3 +25,10 @@ class User(BaseModel):
       name: str
       age: int
       address: Address      
+
+@app.post("/create_user")
+def create_user(user: User):
+    return {
+        "Message": "User created",
+        "data": user
+    }      
