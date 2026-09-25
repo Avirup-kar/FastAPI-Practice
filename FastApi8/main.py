14c2f29674fe84889c32b6e0d812b1ffdc0c9186
Avirup-kar <avirupk57@gmail.com>
2026-09-26 02:17:17 +0530

    craete FastApi8

--- FastApi8/main.py
+++ FastApi8/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from pydantic import BaseModel
 
 app = FastAPI()
+
+@app.post("/create_user", status_code = status.HTTP_201_CREATED)
+def create_user():
+    return {
+        "Message": "User created"
+    }
