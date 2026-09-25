0968ff825f9c9461c445530205e361f660eea55d
Avirup-kar <avirupk57@gmail.com>
2026-09-25 19:44:14 +0530

    craete FastApi4

--- FastApi5/main.py
+++ FastApi5/main.py
@@ -3,9 +3,17 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+todos = []
+
 class Todo(BaseModel):
     id: int
     title: str
     completed: bool
 
-    
+@app.post("/todo")
+def create_todo(todo: Todo):
+    todos.append(todo)
+    return {
+        "Message": "Todo added",
+        "Data": todos
+    }
