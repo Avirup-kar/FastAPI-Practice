be3eaf4cdc11203a07d3827958f859db46f81b0d
Avirup-kar <avirupk57@gmail.com>
2026-09-25 16:53:40 +0530

    craete FastApi4

--- FastApi3/main.py
+++ FastApi3/main.py
@@ -3,12 +3,16 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class user(BaseModel):
+    name: str
+    age: int
+
 # @app.post("/create_user")
 # def create_user(name: str, age: int):
 #     return {"name": name, "age": age}
 
 @app.post("/create_user")
-def create_user(user:dict):
+def create_user(user:user):
     return {
         "Messaeg": "User created successfuly",
         "Data": user
---
+++ FastApi4/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    name: str
+    age: int
