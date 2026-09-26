17749cf35d5f3c5128c94b51124ddae7f5fd0d78
Avirup-kar <avirupk57@gmail.com>
2026-09-27 01:46:57 +0530

    craete FastApi13

--- FastApi13/main.py
+++ FastApi13/main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
@@ -8,3 +8,12 @@ engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
+
+sessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base
+
+class Todod(Base):
+    __tablename__ = "todos"
+    
+    id: Column(Integer, primary_key=True, index=True)
