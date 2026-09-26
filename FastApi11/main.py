cbd90f6092028a14c184f1646babe81b7292cafd
Avirup-kar <avirupk57@gmail.com>
2026-09-26 21:20:11 +0530

    craete FastApi9

---
+++ FastApi11/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, requests
+
+app = FastAPI()
+
+@app.middleware("http")
+async def my_middleware(request: requests, call_next):
+    print("Request Received")
+    
+    responce = call_next(request)
+    
+    print("Responce send")
+    
+    return responce
