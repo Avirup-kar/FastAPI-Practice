5d200f00e8d4c18afe9e0f4fe88ca158522e7309
Avirup-kar <avirupk57@gmail.com>
2026-09-27 02:06:05 +0530

    craete FastApi13

--- FastApi13/main.py
+++ FastApi13/main.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from fastapi import FastAPI
 
 
 DATABASE_URL = "sqlite:///./test.db"
@@ -11,9 +12,14 @@ engine = create_engine(
 
 sessionLocal = sessionmaker(bind=engine)
 
-Base = declarative_base
+Base = declarative_base()
 
 class Todod(Base):
     __tablename__ = "todos"
     
-    id: Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    complted = Column(String)
+    
+    
+Base.metadata.create_all(bind=engine)    
