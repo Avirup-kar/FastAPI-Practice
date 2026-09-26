702809cc8381d6172c6cc177bb457f9231295568
Avirup-kar <avirupk57@gmail.com>
2026-09-26 17:11:09 +0530

    craete FastApi9

--- FastApi9/main.py
+++ FastApi9/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, requests
+from fastapi import FastAPI, HTTPException, Requests
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
@@ -7,6 +7,16 @@ app = FastAPI()
 class UserNotFoundException(Exception):
     def __init__(self, name:str):
           self.name = name
+          
+@app.exception_handler(UserNotFoundException)      
+def user_not_found_handler(reqquest: Requests, exe: UserNotFoundException):
+    return JSONResponse(
+        status_code=404,
+        content={
+            "status":"error",
+            "message": f"User {exe.name} not found"
+        }
+    )    
     
 @app.get("/users/{name}")
 def get_user(name:str):
