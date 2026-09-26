003e56d4d6a93005c24af03bfe808f0ffbfb680f
Avirup-kar <avirupk57@gmail.com>
2026-09-27 00:32:25 +0530

    craete FastApi9

--- FastApi12/main.py
+++ FastApi12/main.py
@@ -2,3 +2,12 @@ import sqlite3
 
 conn = sqlite3.connect("test.db", check_same_thread = False)
 
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS todos (
+    id INTEGER PRIMERY KEY,
+    title TEXT,
+    completed TEXT
+)
+               """)
