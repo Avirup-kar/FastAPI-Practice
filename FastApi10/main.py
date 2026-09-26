a8380e3d3f2704f067b29450036a036869269c5b
Avirup-kar <avirupk57@gmail.com>
2026-09-26 19:39:49 +0530

    craete FastApi9

--- FastApi10/main.py
+++ FastApi10/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Header
 
 app = FastAPI()
 
-def get_current_user():
-    return{
-      "user": "Mohit"
-    }
+# def get_current_user():
+#     return{
+#       "user": "Mohit"
+#     }
+    
+# @app.get("/profile")
+# def profile(user = Depends(get_current_user)):
+#     return user    
+
+
+# @app.get("/dashboard")
+# def dashboard(user = Depends(get_current_user)):
+#     return user    
+
