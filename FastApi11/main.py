71fab7e20353cbb442a59c0548f1249448429312
Avirup-kar <avirupk57@gmail.com>
2026-09-26 22:33:22 +0530

    craete FastApi9

--- FastApi11/main.py
+++ FastApi11/main.py
@@ -1,13 +1,27 @@
 from fastapi import FastAPI, requests
+import time
 
 app = FastAPI()
 
+
 @app.middleware("http")
-async def my_middleware(request: requests, call_next):
-    print("Request Received")
+async def log_middleware(request: requests, call_next):
+    start_time = time.time()
     
     responce = await call_next(request)
     
-    print("Responce send")
+    process_time = time.time()-start_time
+    
+    print(f"Path: {request.url.path} | Time: {process_time}")
     
     return responce
+
+# @app.middleware("http")
+# async def my_middleware(request: requests, call_next):
+#     print("Request Received")
+    
+#     responce = await call_next(request)
+    
+#     print("Responce send")
+    
+#     return responce
